c6173f3544dc62f519637f068186652b70b346ac
kkuga <k.kugasanth@gmail.com>
2015-11-29 14:54:55 +0100

    authendification fonctionnel avec gestion des sessions users

--- app/src/main/java/subwaybuddy/miage/paris10/com/subapp/LoginActivity.java
+++ app/src/main/java/subwaybuddy/miage/paris10/com/subapp/LoginActivity.java
@@ -6,18 +6,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import subwaybuddy.miage.paris10.com.subapp.libraries.DashboardActivity;
 import subwaybuddy.miage.paris10.com.subapp.libraries.HttpRequestsApi;
 
 public class LoginActivity extends AppCompatActivity
 {
     EditText ET_NAME,ET_PASS;
     String login_name,login_pass;
+    // Session Manager Class
+    SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +33,8 @@ public class LoginActivity extends AppCompatActivity
         setContentView(R.layout.activity_login);
         ET_NAME = (EditText)findViewById(R.id.user_name);
         ET_PASS = (EditText)findViewById(R.id.user_pass);
-
+        // Session Manager
+        session = new SessionManager(getApplicationContext());
     }
 
     public void userReg(View view)
@@ -33,8 +42,7 @@ public class LoginActivity extends AppCompatActivity
         startActivity(new Intent(this,Register.class));
     }
 
-    public void userLogin(View view) throws IOException
-    {
+    public void userLogin(View view) throws IOException, JSONException {
         login_name = ET_NAME.getText().toString();
         login_pass = ET_PASS.getText().toString();
         //BackgroundTask backgroundTask = new BackgroundTask(this);
@@ -42,10 +50,29 @@ public class LoginActivity extends AppCompatActivity
         // on POST le contenu des champs textes pour authentifier l'utilisateur.
         String url          = "http://christian-hiroz.com/SubwayBuddy/web/app_dev.php/users/login" ;
         HttpRequestsApi api = new HttpRequestsApi( url ) ;
-        String reponse      = api.post( "username=" + login_name + "&password=" + login_pass );
-        // @TODO Kuga : parser le JSON pour voir le résultat
-        // par exemple, si tu trouves "id" dans le JSON, c'est que l'utilisateur est authentifié
-        // dans ce cas, préparer la suite.
-        System.out.println( reponse );
+        String reponse      = api.post("username=" + login_name + "&password=" + login_pass);
+        System.out.println(reponse );
+        if(reponse.equalsIgnoreCase("0")){
+            Toast toast = Toast.makeText(this, "Authendification échoué, veuillez réessayer svp", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+        else {
+            JSONObject json = new JSONObject(reponse);
+            String str_valueId = json.getString("id");// on récupère l'id de l'user
+            String str_valueUsername = json.getString("username");// on récupère l'id de l'user
+            String str_valueEmail = json.getString("email");// on récupère l'id de l'user
+
+            String tempreponse = str_valueId;
+            System.out.println("id" + str_valueId);
+            // Creating user login session
+            session.createLoginSession(str_valueUsername, str_valueEmail);
+            startActivity(new Intent(this, DashboardActivity.class));
+
+
+        }
+
+
+
     }
 }
