0dc27daccd30d3f17a0e3087f0273400c4f3cbab
kkuga <k.kugasanth@gmail.com>
2015-12-02 12:31:49 +0100

    Mes Voyages Activity

---
+++ app/src/main/java/subwaybuddy/miage/paris10/com/subapp/TravelViewActivity.java
@@ -0,0 +1,13 @@
+package subwaybuddy.miage.paris10.com.subapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TravelViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_view);
+    }
+}
